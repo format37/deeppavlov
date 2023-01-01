0408a9e9d74d7c9af87a40aa91b166776bcf0050
alex <format37@gmail.com>
2023-01-01 19:02:38 +0400

    modified:   test.py

--- data/huggingface/test.py
+++ data/huggingface/test.py
@@ -1,10 +1,11 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 tokenizer = AutoTokenizer.from_pretrained("bigscience/T0pp")
 model = AutoModelForSeq2SeqLM.from_pretrained("bigscience/T0pp")
-# Read prompt from file
-with open("prompt.txt", "r") as f:
-    prompt = f.read()
-inputs = tokenizer.encode(prompt, return_tensors="pt")
-outputs = model.generate(inputs)
-print(tokenizer.decode(outputs[0]))
+for i in range(3):
+    with open("prompt_"+str(i)+".txt", "r") as f:
+        print('===', i)
+        prompt = f.read()
+        inputs = tokenizer.encode(prompt, return_tensors="pt")
+        outputs = model.generate(inputs)
+        print(tokenizer.decode(outputs[0]))
 print('done')
