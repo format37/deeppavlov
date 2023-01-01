3c7a120a95b972f4405f286adbeb599de88e5321
alex <format37@gmail.com>
2023-01-01 19:08:08 +0400

    modified:   test.py

--- data/huggingface/test.py
+++ data/huggingface/test.py
@@ -1,6 +1,9 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+print('loading tokenizer and model')
 tokenizer = AutoTokenizer.from_pretrained("bigscience/T0pp")
+print('loading model')
 model = AutoModelForSeq2SeqLM.from_pretrained("bigscience/T0pp")
+print('generating')
 for i in range(3):
     with open("prompt_"+str(i)+".txt", "r") as f:
         print('===', i)
