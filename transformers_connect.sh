b1c7a33ca297db806f41cbd9ac2dcb7eb1a5c19c
alex <format37@gmail.com>
2023-01-01 14:05:46 +0400

    modified:   transformers_connect.sh

--- transformers_connect.sh
+++ transformers_connect.sh
@@ -3,7 +3,7 @@
 ###
 
 # get id of running container with image name "nlp_transformers"
-container_id=$(docker ps | grep nlp_transformers | awk '{print $1}')
+container_id=$(sudo docker ps | grep nlp_transformers | awk '{print $1}')
 echo "container id: $container_id"
 # connect to container
 sudo docker exec -it $container_id /bin/bash
